576905e963bcb0811323edcc28f3d70ed5e539d6
deevroman <roman.deev06@gmail.com>
2019-11-09 09:21:11 +0300

    Add clear tree

--- cmake-build-debug/CMakeFiles/myqtproject_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/myqtproject_autogen"
-  "myqtproject_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/myqtproject_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
